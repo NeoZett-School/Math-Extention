9509445c33b9dc2921de9cc875d100074a50f6a3
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2026-03-28 16:31:29 +0100

    Introduced the next implementation in the test.

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
-from math_extention import Canvas, Symbol, Function, Solver, SystemSolver
+from math_extention import Canvas, Symbol, Function, Solver, SymbolLike
 
 canvas = Canvas()
-x = Symbol("x")
-y = Symbol("y")
-eq1 = x**2 + y**2 - 1
-eq2 = y - x
+x = Symbol('x')
+f = Function('x', x**3 - 6*x**2 + 9*x + 15)
 
-solver = SystemSolver()
-# Guessing 0.5 for both should lead to sqrt(2)/2 (~0.707)
-result = solver.solve_nonlinear([eq1, eq2], [x, y], [0.5, 0.5])
-print(result)
+# 3x**2 - 12x + 9 = 0
+# x = 1, x = 3
+
+def extreme(func: Function, symbol: SymbolLike) -> None:
+    df = func.get_derivative()
+    print(f'Function: {func.written}')
+    print(f'First derivative: {df.written}')
+    x = Solver.solve(
+        expr = df, 
+        target = 0, 
+        symbol = symbol,
+    )
+    y = f(x)
+    if y > 0:
+        print(f'Maximum at x={(x, y)}')
+    elif y < 0:
+        print(f'Minimum at x={(x, y)}')
+    else:
+        print(f'Inflection point at x={(x, y)}')
+
+extreme(f, x)
