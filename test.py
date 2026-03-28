e36226f03a51431e422eccc6ec84d20acc5d5e71
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2026-03-28 16:59:49 +0100

    I removed the default search range, since it did not very well in finding the points.

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ f = Function('x', x**3 - 6*x**2 + 9*x + 15)
 
 print("Function:", f.written)
 
-extremes = Solver.find_extrema(expr=f, symbol=x, search_range=(-10, 10))
+extremes = Solver.find_extrema(expr=f, symbol=x, search_range=(-5, 5))
 print("Extremes:", extremes)
