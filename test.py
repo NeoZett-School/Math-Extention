2a1e067a7d1748da69492439a53e7dced435abfc
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2026-04-06 13:20:32 +0200

    Trying out constant numbers in the smoke test. Remeber that equal comparison will only turn into an equation if both parts are traceable objects. If the right side is a real number in python coding--intiger or floating--it will return a boolean value to wether the treaceable calculates into that number.

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 from math_extension import Canvas, Symbol, Solver, Traceable, Equation
 
 canvas = Canvas()
-x = Symbol('x', 2)
+x = Symbol('x', 0)
 
 expr = Traceable.parse("x^2 + 3*x + 2")
+equa = Equation.parse(expr == Traceable.parse(2))
+print(equa.are_equal())
